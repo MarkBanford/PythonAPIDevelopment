184093a3b9faf03335c165cdcf57198d1ce1badd
Mark Banford <markbanford@gmail.com>
2023-07-08 18:12:18 +0100

    ic1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''uvicorn main:app --reload
-POSTMAN app for testing
+POSTMAN app for testing  1:14
 '''
 
 from fastapi import FastAPI
@@ -13,6 +13,7 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
 
 
 @app.get("/")  # Uses get method and "/" path
@@ -27,7 +28,7 @@ def get_posts():
 
 @app.post("/createposts")
 def create_posts(new_post: Post):
-    print(new_post)
+    print(new_post.published)
     return {"data": "new post"}
 
 # title str, content str
