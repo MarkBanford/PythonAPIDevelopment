ffc7e353a4df475fc739cb6d2cd84ea59f802bb9
Mark Banford <markbanford@gmail.com>
2023-07-11 20:32:54 +0100

    ic1

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
 import requests
 
-print(requests.get("http://127.0.0.1:8000/items/0").json())
+# print(requests.get("http://127.0.0.1:8000/items/0").json())
+print(requests.get("http://127.0.0.1:8000/items?name=Nails").json())
+print(
+    requests.post("http://127.0.0.1:8000/",
+                  json={'name': 'Screwdriver', 'price': 3.99, 'count': 10, 'id': 4, 'category': 'tools'}).json())
+
+print(requests.put("http://127.0.0.1:8000/items/0?count=9001").json())
+print(requests.get("http://127.0.0.1:8000/").json())
+
+print(requests.delete("http://127.0.0.1:8000/items/0").json())
+print(requests.get("http://127.0.0.1:8000/").json())
